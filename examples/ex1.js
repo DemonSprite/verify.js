969a32a3fcdb0ff48fc5a10fd6e008c9f6903099
Craig Condon <craig@spiceapps.com>
2012-10-26 18:00:43 -0500

    initial commit

--- examples/ex1.js
+++ examples/ex1.js
@@ -1,11 +1,15 @@
 var verify = require("../")();
 
-verify.register("email2").is("email").len(10)
+verify.register("email2").is("email").len(10);
+verify.register("name").len(2);
 
 
 var success = verify.check({
-	email: "me@email.com"
-}).has("email2").
+	email: "me@email.com",
+	name: "craig"
+}).onError(function(error) {
+	console.error(error)
+}).has("email", "name").
 success;
 
 console.log(success)
